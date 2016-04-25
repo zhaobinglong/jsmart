a3ff5029362b3453ac8d1c21bd05aa444595db0b
rand.zhao <rand.zhao@estay.com>
2016-04-25 17:17:28 +0800

    add  增加数组去重的函数

--- jsmart.js
+++ jsmart.js
@@ -16,3 +16,20 @@ jsmart.comdify=function(str)
   var n1=str.replace(/^(\d+)((\.\d+)?)$/,function(s,s1,s2){return s1.replace(re,"$&,")+s2;});
   return n1;
 }
+
+/**
+ * 数组去重 
+ * @param  {Array} arr 参数字符串
+ * @return {Araay}    
+ */
+ jsmart.Arrunique=function(arr) {
+    var result = [], hash = {};
+    for (var i = 0, elem; (elem = arr[i]) != null; i++) {
+        if (!hash[elem]) {
+            result.push(elem);
+            hash[elem] = true;
+        }
+    }
+    return result;
+}
+
