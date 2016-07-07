b014b5c84b31986058acb080965f9879e8ab7cb6
rand.zhao <rand.zhao@estay.com>
2016-07-07 11:37:56 +0800

    add  增加扇形绘制函数

--- jsmart.js
+++ jsmart.js
@@ -1,3 +1,4 @@
+'use strict'
 // jsmart lib;
 // 1.0.0
 
@@ -11,16 +12,16 @@ var jsmart={};
  */
 jsmart.comdify=function(str)
 {
-  str = str.substring(0, 10);                 
-  var re=/\d{1,3}(?=(\d{3})+$)/g;      
+  str = str.substring(0, 10);
+  var re=/\d{1,3}(?=(\d{3})+$)/g;
   var n1=str.replace(/^(\d+)((\.\d+)?)$/,function(s,s1,s2){return s1.replace(re,"$&,")+s2;});
   return n1;
 }
 
 /**
- * 数组去重 
+ * 数组去重
  * @param  {Array} arr 参数字符串
- * @return {Araay}    
+ * @return {Araay}
  */
  jsmart.arrUnique=function(arr) {
     var result = [], hash = {};
@@ -50,9 +51,9 @@ jsmart.shuffle=function(arr){
 }
 
 /**
- * 将字符串内的全角符号转换成半角符号 
+ * 将字符串内的全角符号转换成半角符号
  * @param  {String} str 字符串
- * @return {String}    
+ * @return {String}
  */
  jsmart.toSBC=function(str){
     var result = "";
@@ -72,10 +73,10 @@ jsmart.shuffle=function(arr){
 /**
  * 将字符串中的单词首字母转大写
  * @param  {String} str 字符串
- * @return {String}    
+ * @return {String}
  */
  jsmart.titleCase=function(str) {
-     return str.replace(/\w\S*/g, function(word){ 
+     return str.replace(/\w\S*/g, function(word){
              return word.charAt(0).toUpperCase()+word.substr(1).toLowerCase();
      });
  }
@@ -97,7 +98,7 @@ jsmart.shuffle=function(arr){
 /**
  * 判断字符串是不是整数(从0开始的整数)
  * @param  {String} name 参数值
- * @return {bool}      
+ * @return {bool}
  */
  jsmart.isInteger=function(str){
    return /^\+?\d+$/.test(str);
@@ -107,7 +108,7 @@ jsmart.shuffle=function(arr){
  * 利用正则判断字符串是否存在子串  不区分大小写
  * @param  {String} subStr  子串
  * @param  {String} str     父串
- * @return {Bool}   
+ * @return {Bool}
  */
 jsmart.coverString=function(subStr,str){
    var reg = eval("/"+subStr+"/ig");
@@ -117,14 +118,14 @@ jsmart.coverString=function(subStr,str){
 /**
  * 利用递归实现阶乘
  * @param  {Number} num 一个整数
- * @return {Number}      
+ * @return {Number}
  */
  jsmart.factorial=function (num) {
-    if (num < 0) {       
+    if (num < 0) {
         return -1;
-    } else if (num === 0 || num === 1) {      
+    } else if (num === 0 || num === 1) {
          return 1;
-    } else {        
+    } else {
       return (num * factorial(num - 1));
     }
 }
@@ -136,43 +137,43 @@ jsmart.coverString=function(subStr,str){
  * @param {[type]} endRow   [description]
  * @param {[type]} col      [description]
  */
-jsmart.MergeCell=function(tableId, startRow, endRow, col) {  
-    var tb = document.getElementById(tableId);  
-    if (col >= tb.rows[0].cells.length) {  
-        return;  
-    }  
-    //当检查第0列时检查所有行  
-    if (col == 0) {  
-        endRow = tb.rows.length - 1;  
-    }  
-    for (var i = startRow; i < endRow; i++) {  
-        //subCol:已经合并了多少列  
-        var subCol = tb.rows[0].cells.length - tb.rows[startRow].cells.length;  
-        //程序是自左向右合并，所以下一行一直取第0列  
-        if (tb.rows[startRow].cells[col - subCol].innerHTML == tb.rows[i + 1].cells[0].innerHTML) {  
-            //如果相同则删除下一行的第0列单元格  
-            tb.rows[i + 1].removeChild(tb.rows[i + 1].cells[0]);  
-            //更新rowSpan属性  
-            tb.rows[startRow].cells[col - subCol].rowSpan = (tb.rows[startRow].cells[col - subCol].rowSpan | 0) + 1;  
-            //当循环到终止行前一行并且起始行和终止行不相同时递归(因为上面的代码已经检查了i+1行，所以此处只到endRow-1)  
-            if (i == endRow - 1 && startRow != endRow) {  
-                MergeCell(tableId, startRow, endRow, col + 1);  
-            }  
-        } else {  
-            //起始行，终止行不变，检查下一列  
-            MergeCell(tableId, startRow, i, col + 1);  
-            //增加起始行  
-            startRow = i + 1;  
-        }  
-    }  
-}  
+jsmart.MergeCell=function(tableId, startRow, endRow, col) {
+    var tb = document.getElementById(tableId);
+    if (col >= tb.rows[0].cells.length) {
+        return;
+    }
+    //当检查第0列时检查所有行
+    if (col == 0) {
+        endRow = tb.rows.length - 1;
+    }
+    for (var i = startRow; i < endRow; i++) {
+        //subCol:已经合并了多少列
+        var subCol = tb.rows[0].cells.length - tb.rows[startRow].cells.length;
+        //程序是自左向右合并，所以下一行一直取第0列
+        if (tb.rows[startRow].cells[col - subCol].innerHTML == tb.rows[i + 1].cells[0].innerHTML) {
+            //如果相同则删除下一行的第0列单元格
+            tb.rows[i + 1].removeChild(tb.rows[i + 1].cells[0]);
+            //更新rowSpan属性
+            tb.rows[startRow].cells[col - subCol].rowSpan = (tb.rows[startRow].cells[col - subCol].rowSpan | 0) + 1;
+            //当循环到终止行前一行并且起始行和终止行不相同时递归(因为上面的代码已经检查了i+1行，所以此处只到endRow-1)
+            if (i == endRow - 1 && startRow != endRow) {
+                MergeCell(tableId, startRow, endRow, col + 1);
+            }
+        } else {
+            //起始行，终止行不变，检查下一列
+            MergeCell(tableId, startRow, i, col + 1);
+            //增加起始行
+            startRow = i + 1;
+        }
+    }
+}
 
 /**
  * 合并横向的单元格 空白的单元格和有内容的单元格合并  可以指定合并的列
  * @param {[type]} tableId   [description]
  * @param {[type]} startRCol 开始合并的列
  * @param {[type]} endCol    结束合并的列
- * 
+ *
  */
 jsmart.MergeColCell=function(tableId,startCol,endCol){
    var trs=$('#'+tableId).find('tr');
@@ -200,10 +201,10 @@ jsmart.MergeColCell=function(tableId,startCol,endCol){
  * @return {Boolean}   [description]
  */
 jsmart.isEmptyObject=function(e){
-  var t;  
-  for (t in e)  
-      return !1;  
-  return !0  
+  var t;
+  for (t in e)
+      return !1;
+  return !0
 }
 
 /**
@@ -228,5 +229,30 @@ jsmart.showBase64img=function(type,imgString){
 }
 
 
-
-
+/**
+ *  绘制扇形函数
+ * @param {[type]} id        canvas的id
+ * @param {[type]} x         圆心的x坐标
+ * @param {[type]} y         圆心的y坐标
+ * @param {[type]} radius    扇形半径
+ * @param {[type]} color     填充的颜色
+ * @param {[number]} eAngle    扇形结束的角度 0 - 360
+ *
+ */
+jsmart.sector=function(id,x,y,radius,color,eAngle){
+	var canvas = document.getElementById(id);
+	var ctx = canvas.getContext('2d');
+	// 开始一条新路径
+	ctx.beginPath();
+	// 位移到圆心，方便绘制
+	ctx.translate(x, y);
+	// 移动到圆心
+	ctx.moveTo(0, 0);
+	// 绘制圆弧
+	ctx.arc(0, 0, radius, 0, Math.PI * (eAngle/180));
+	// 闭合路径
+	ctx.closePath();
+	// 填充颜色
+	ctx.fillStyle=color;
+	ctx.fill();
+}
